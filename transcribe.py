c3a0e2dabb9aa65cc56d71807f5e4397966e190f
sgp715 <7sebastianperez@gmail.com>
2017-02-25 02:44:06 -0600

    making it have a return val

--- transcribe.py
+++ transcribe.py
@@ -10,9 +10,12 @@ def audio_to_text(file_path):
     with sr.AudioFile(AUDIO_FILE) as source:
         audio = r.record(source)
 
+    text = None
     try:
-        print("Sphinx thinks you said: " + r.recognize_sphinx(audio))
+        text = r.recognize_sphinx(audio)
     except sr.UnknownValueError:
         print("Sphinx could not understand audio")
     except sr.RequestError as e:
         print("Sphinx error; {0}".format(e))
+
+    return text
