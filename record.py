a4d7346065a45bd97a76b5eeb7c571805252d295
sgp715 <7sebastianperez@gmail.com>
2017-02-25 09:33:17 -0600

    record

--- record.py
+++ record.py
@@ -6,44 +6,33 @@ CHUNK = 1024
 FORMAT = pyaudio.paInt16 #paInt8
 CHANNELS = 2
 RATE = 16000 #sample rate
-RECORD_SECONDS = 4
+RECORD_SECONDS = 10
 WAVE_OUTPUT_FILENAME = "output.wav"
 
-class Recorder(object):
 
-    def __init__(self):
-        self.p = pyaudio.PyAudio()
+def record_and_save(file_path):
 
-    def recorde_audio(self):
+    p = pyaudio.PyAudio()
 
-        stream = self.p.open(format=FORMAT,
+    frames = []
+    stream = p.open(format=FORMAT,
                         channels=CHANNELS,
                         rate=RATE,
                         input=True,
-                        frames_per_buffer=CHUNK) #buffer
+                        frames_per_buffer=CHUNK)
 
-        print("* recording")
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data) # 2 bytes(16 bits) per channel
 
-        frames = []
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
-        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-            data = stream.read(CHUNK)
-            frames.append(data) # 2 bytes(16 bits) per channel
-
-        print("* done recording")
-
-        stream.stop_stream()
-        stream.close()
-        self.p.terminate()
-
-        return frames
-
-    def save_audio(self, frames, file_path):
-
-        # TODO: make a temp file
-        wf = wave.open(file_path, 'wb')
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(self.p.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
-        wf.writeframes(b''.join(frames))
-        wf.close()
+    # TODO: make a temp file
+    wf = wave.open(file_path, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
