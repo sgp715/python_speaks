c0652a9d56176ca8bc20f237c0cff7bbecc6b6cd
sgp715 <7sebastianperez@gmail.com>
2017-02-25 18:43:54 -0600

    that's as good as it gets with this pos microphone

--- record.py
+++ record.py
@@ -1,12 +1,18 @@
 import pyaudio
 import wave
+from collections import deque
+import math
+import audioop
 
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16 #paInt8
-CHANNELS = 2
+CHANNELS = 1
 RATE = 16000 #sample rate
 RECORD_SECONDS = 7
+THRESHOLD = 6000
+SILENCE_LIMIT = 1
+PREV_AUDIO = 0.5
 
 
 def record_and_save(file_path):
@@ -14,6 +20,11 @@ def record_and_save(file_path):
     p = pyaudio.PyAudio()
 
     frames = []
+    cur_data = ''
+    rel = RATE/CHUNK
+    slid_win = deque(maxlen=SILENCE_LIMIT * rel)
+    prev_audio = deque(maxlen=PREV_AUDIO * rel)
+    started = False
 
     stream = p.open(format=FORMAT,
                         channels=CHANNELS,
@@ -21,18 +32,64 @@ def record_and_save(file_path):
                         input=True,
                         frames_per_buffer=CHUNK)
 
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
+    while True:
+        cur_data = stream.read(CHUNK)
+        slid_win.append(math.sqrt(abs(audioop.avg(cur_data, 4))))
+
+        if(sum([x > THRESHOLD for x in slid_win]) > 0):
+            print 't: ' + str(THRESHOLD)
+            print 'x: ' + str(x)
+            print started
+            if(not started):
+                print "Starting record of phrase"
+                started = True
+            frames.append(cur_data)
+        elif (started is True):
+            wf = wave.open(file_path, 'wb')
+            wf.setnchannels(CHANNELS)
+            wf.setsampwidth(p.get_sample_size(FORMAT))
+            wf.setframerate(RATE)
+            wf.writeframes(b''.join(list(prev_audio) + frames))
+            wf.close()
+            slid_win = deque(maxlen=SILENCE_LIMIT * rel)
+            prev_audio = deque(maxlen=0.5 * rel)
+            break
+        else:
+            frames.append(cur_data)
+
+    # for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    #     data = stream.read(CHUNK)
+    #     frames.append(data)
 
     stream.stop_stream()
     stream.close()
     p.terminate()
 
-    # TODO: make a temp file
-    wf = wave.open(file_path, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
+
+def audio_int(num_samples=50):
+    """ Gets average audio intensity of your mic sound. You can use it to get
+        average intensities while you're talking and/or silent. The average
+        is the avg of the 20% largest intensities recorded.
+    """
+
+    print "Getting intensity values from mic."
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    values = [math.sqrt(abs(audioop.avg(stream.read(CHUNK), 4)))
+              for x in range(num_samples)]
+    values = sorted(values, reverse=True)
+    r = sum(values[:int(num_samples * 0.2)]) / int(num_samples * 0.2)
+    print " Finished "
+    print " Average audio intensity is ", r
+    stream.close()
+    p.terminate()
+
+
+if(__name__ == '__main__'):
+    audio_int()  # To measure your mic levels
